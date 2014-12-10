1d034793d45f6e38fbe403ada238c18b0779b8f7
Zachary <zbridges@gmail.com>
2014-12-10 13:04:55 -0500

    updated TODO, added ww function for www search

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,19 @@
 #! /bin/zsh
 
 dot() {
-cd ~/.dotfiles
+	cd ~/.dotfiles/$@
+}
+
+ww(){
+	local url="https://www.duckduckgo.com"
+	if (( $# > 0 ))
+       	then url="${url}/?q=";
+	       	while [[ $# -gt 0 ]]; do 
+			url="${url}$1+"; 
+			shift; 
+		done; 
+		url=${url%?}; 
+	fi
+
+	xdg-open "$url" 2> /dev/null
 }
