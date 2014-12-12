1a1a505ea73eb105e35507798fc1703f2f95f8d5
Zachary Bridges <zbridges@gmail.com>
2014-12-12 17:18:20 -0500

    added git branch info to prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,4 +2,27 @@
 PROMPT="%~ %F{160}>>%f%F{136};--%f%F{160>%f "
 
 # Right-handed prompt
-RPROMPT=">>[%T]<<"
+#RPROMPT=">>[%T]<<"
+
+# Up-to-the-minute time in RPROMPT
+#TMOUT=15
+#TRAPALRM () {
+#zle reset-prompt
+#}
+
+setopt prompt_subst
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' actionformats 	'%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+zstyle ':vcs_info:*' formats		'%F{5}[%F{2}%b%F{5}]%f '
+#zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+
+zstyle ':vcs_info:*' enable git
+
+# or use pre_cmd, see man zshcontrib
+vcs_info_wrapper() {
+	vcs_info
+	if [ -n "$vcs_info_msg_0_" ]; then
+		echo "${vcs_info_msg_0_}%{$reset_color%}$del"
+	fi
+}
+RPROMPT=$'$(vcs_info_wrapper)'
