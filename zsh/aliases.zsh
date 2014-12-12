505dc4c9fc35d5c4b43b7eea3084de93c3f79fdc
Zachary Bridges <zbridges@gmail.com>
2014-12-11 19:32:18 -0500

    updated i3/config and zsh/aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,4 +22,5 @@ alias cim="vim"
 # Git aliases, a few borrowed from Oh My Zsh!
 alias g="git"
 alias gl="git log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short --max-count=10" 
-alias ga="_git-add"
+alias ga="_git-add" # from functions.zsh
+alias gc="git commit"
