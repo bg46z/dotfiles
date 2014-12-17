6fffa992080baab3c32b7870a2eefdb9af93a8d1
Zachary Bridges <ztb11@swarm.biophysics.fsu.edu>
2014-12-17 13:00:00 -0500

    cleaned up project

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Starting install..."
-git submodule --recursive init
-git submodule --recursive update
--- update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Updating..."
-git submodule foreach git pull origin master
