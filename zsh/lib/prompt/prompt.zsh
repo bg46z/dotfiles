8fdabf179b47f920ef7f55a7eb2a8d462bf2f1e9
Zachary Bridges <zbridges@gmail.com>
2015-01-12 10:52:15 -0500

    added numpad support to i3

--- zsh/lib/prompt/prompt.zsh
+++ zsh/lib/prompt/prompt.zsh
@@ -1,29 +1,18 @@
 # Left-handed prompt
-PROMPT="%~ %F{160}>>%f%F{136};--%f%F{160>%f "
+PROMPT="%2~ %F{160}>>%f%F{136};--%f%F{160>%f "
+PROMPT2="%~ %F{160}>>%f%F{136};--%f%F{160>%f "
 
-# Right-handed prompt
-#RPROMPT=">>[%T]<<"
 
 # Up-to-the-minute time in RPROMPT
-#TMOUT=15
-#TRAPALRM () {
-#zle reset-prompt
-#}
+TMOUT=15
+TRAPALRM () {
+zle reset-prompt
+}
 
 setopt prompt_subst
-autoload -Uz vcs_info
 zstyle ':vcs_info:*' actionformats 	'%F{160}>>[%F{136}%b|%a%F{160}]<<%f'
 zstyle ':vcs_info:*' formats		'%F{160}>>[%F{136}%b%F{160}]<<%f'
 zstyle ':vcs_info:*' enable git
 
-# or use pre_cmd, see man zshcontrib
-#vcs_info_rprompt() {
-	#vcs_info
-	#if [ -n "$vcs_info_msg_0_" ]; then
-		#echo "${vcs_info_msg_0_}"
-		##echo "${vcs_info_msg_0_}%{$reset_color%}$del"
-	#else
-		#echo "%F{160}>>[%F{136}%T%F{160}]<<%f"
-	#fi
-#}
+# Right-handed prompt
 RPROMPT=$'$(vcs_info_rprompt)'
