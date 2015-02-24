4e580d772bf99581aa4f0345b8f4227bfa2a2d2e
Zachary Bridges <zbridges@gmail.com>
2015-02-24 14:08:50 -0500

    added root prompt

--- zsh/lib/prompt/prompt.zsh
+++ zsh/lib/prompt/prompt.zsh
@@ -1,7 +1,10 @@
 # Left-handed prompt
-PROMPT="%2~ %F{160}>>%f%F{136};--%f%F{160>%f "
-PROMPT2="%~ %F{160}>>%f%F{136};--%f%F{160>%f "
-
+if [[ $(id -u) -ne 0 ]]; then
+	PROMPT="%2~ %F{160}>>%f%F{136};--%f%F{160>%f "
+	PROMPT2="%~ %F{160}>%f "
+fi
+if [[ $(id -u) -eq 0]]; then
+	PROMPT="%F{214}%U%Broot%b%u@%m %~ %#%f "
 
 # Up-to-the-minute time in RPROMPT
 TMOUT=15
