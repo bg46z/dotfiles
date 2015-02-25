ca7821aaddbc8b39cede0980ab3187a61e3119a9
Zachary Bridges <zbridges@gmail.com>
2015-02-25 14:49:12 -0500

    added newpost function, got rid of lib dir
    The lib directory didn't really make sense for my purposes. Things aren't quite as modular as Oh-My-Zsh.

--- misc/jekyll-new-post.rb
+++ misc/jekyll-new-post.rb
@@ -1,6 +1,28 @@
 #!/usr/bin/env ruby
+require 'optparse'
+
+options = {}
+options[:blog_dir] = ""
+
+OptionParser.new do |opts|
+	opts.banner = "Usage: jekyll-new-post.rb [options] name of post separated by spaces"
+	opts.on("-h", "--help", "Print this help") do
+		puts opts
+		exit
+	end
+	opts.on("-b", "--blog-dir DIRECTORY", "specify the base directory of your jekyll blog.") do |b|
+		options[:blog_dir] = b
+	end
+end.parse!
+
+if options[:blog_dir] != ""
+	posts_dir="#{options[:blog_dir]}/_posts/"
+else
+
+end
+
 date = Time.new.strftime("%Y-%m-%d").to_s
-filename = "#{date}"
+filename = "#{posts_dir}#{date}"
 title = ""
 ARGV.each do |a|
 	 title = title + '-' + a.chomp()
