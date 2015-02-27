b88fe81dc0da78bc1451b880b6dc2542d7f04927
Zachary Bridges <zbridges@gmail.com>
2015-02-27 11:45:07 -0500

    updated zshenv, added vimpager
    Signed-off-by: Zachary Bridges <zbridges@gmail.com>

--- zsh/aliases/system.zsh
+++ zsh/aliases/system.zsh
@@ -6,21 +6,7 @@ alias la="l -la"
 alias grep="grep --color=auto"
 alias rest='echo "rebooting..."; sudo shutdown -r now >& -'
 alias shut='echo "powering off..."; sudo shutdown -h now >& -'
+alias less="$PAGER"
+alias zless="$PAGER"
+alias more="$PAGER"
 
-# Apt-get aliases
-# TODO check OS before loading aliases
-alias ag="sudo apt-get"
-alias agu="ag update"
-alias agd="ag dist-upgrade"
-alias agi="ag install"
-alias ac="apt-cache"
-alias acs="ac search"
-
-# Typo correction...
-alias cim="vim"
-
-# Git aliases, a few borrowed from Oh My Zsh!
-alias g="git"
-alias gl="git log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short --max-count=10" 
-alias ga="_git-add" # from functions.zsh
-alias gc="git commit"
